0cea1a182aa263b1869cdc052725cb857d681c8b
Eduardo Gonzalez <eduardo.gonzalez.flores28@gmail.com>
2021-05-11 21:19:24 -0500

    Subiendo repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {ThemeService,Theme } from './services/theme.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+
+  theme: Observable<Theme>;
+
+  constructor (private themeService: ThemeService){
+  }
+
+  ngOnInit(){
+    this.theme= this.themeService.mode$;
+  }
+
+  toggleTheme(){
+    this.themeService.toggleMode();
+  }
+}
